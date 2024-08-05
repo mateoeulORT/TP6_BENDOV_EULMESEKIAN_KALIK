cbea8bd827dc3230e2aaf1b378a327f04145206f
mateoeulORT <48044168@est.ort.edu.ar>
2024-08-05 10:00:59 -0300

    Empezar proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP06_BenDov_Eulmesekian_Kalik.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
