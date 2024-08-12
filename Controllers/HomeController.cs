41bd1966ebb30b6d39dd347ba61e1a6e306384f2
mateoeulORT <48044168@est.ort.edu.ar>
2024-08-12 19:05:00 -0300

    layout terminado y carrousel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -67,6 +67,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Historia()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
